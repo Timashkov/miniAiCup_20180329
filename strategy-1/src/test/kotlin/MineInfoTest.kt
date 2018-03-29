67fec1cf4667727fa0e9cb2222f63c91c4581324
Timashkov Aleksey <osb.zla@gmail.com>
2018-03-29 23:15:15 +0300

    refactored

--- strategy-1/src/test/kotlin/MineInfoTest.kt
+++ strategy-1/src/test/kotlin/MineInfoTest.kt
@@ -1,13 +1,14 @@
+import incominginfos.MineInfo
 import org.json.JSONArray
 
-class MineStateTest {
+class MineInfoTest {
 
-    lateinit var mMineState: MineState
+    lateinit var mMineState: MineInfo
 
     @org.junit.Before
     fun setUp() {
         val jsarr = JSONArray("[{\"Id\":\"1\",\"M\":40,\"R\":12.649110640673518,\"SX\":-0.92513099579326519,\"SY\":-0.34741206171139455,\"TTF\":31,\"X\":473.07486900420673,\"Y\":177.6525879382886}]")
-        mMineState = MineState(jsarr)
+        mMineState = MineInfo(jsarr)
     }
 
     @org.junit.After
