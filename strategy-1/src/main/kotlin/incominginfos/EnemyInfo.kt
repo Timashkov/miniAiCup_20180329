da87c33f057faf9473ba65a4e63611b13c4bd724
Timashkov Aleksey <osb.zla@gmail.com>
2018-04-01 23:09:41 +0300

    wrap enemy info to vertex

--- strategy-1/src/main/kotlin/incominginfos/EnemyInfo.kt
+++ strategy-1/src/main/kotlin/incominginfos/EnemyInfo.kt
@@ -1,11 +1,11 @@
 package incominginfos
 
 import org.json.JSONObject
+import utils.Vertex
 
 class EnemyInfo(enemyJson: JSONObject) {
+    val mVertex = Vertex(enemyJson.getFloat("X"), enemyJson.getFloat("Y"))
     val mId: String = enemyJson.getString("Id")
-    val mX: Float = enemyJson.getFloat("X")
-    val mY: Float = enemyJson.getFloat("Y")
     val mMass: Float = enemyJson.getFloat("M")
     val mRadius: Float = enemyJson.getFloat("R")
 }
