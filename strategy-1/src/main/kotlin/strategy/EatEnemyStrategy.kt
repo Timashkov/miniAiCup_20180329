3a1443c80479e8845e07946869fbdb3e153e3ec1
Timashkov Aleksey <osb.zla@gmail.com>
2018-04-05 23:05:08 +0300

    partially fixed eat enemy

--- strategy-1/src/main/kotlin/strategy/EatEnemyStrategy.kt
+++ strategy-1/src/main/kotlin/strategy/EatEnemyStrategy.kt
@@ -1,16 +1,17 @@
 package strategy
 
 import WorldConfig
+import data.ParseResult
 import utils.GameEngine
 import utils.Logger
 import utils.Vertex
 
 class EatEnemyStrategy(val mGlobalConfig: WorldConfig, val mLogger: Logger) : IStrategy {
 
-    override fun apply(gameEngine: GameEngine): StrategyResult {
+    override fun apply(gameEngine: GameEngine, cachedParseResult: ParseResult?): StrategyResult {
 
         if (gameEngine.worldParseResult.worldObjectsInfo.mEnemies.isNotEmpty()) {
-            return searchForEnemies(gameEngine)
+            return searchForEnemies(gameEngine, cachedParseResult)
         }
 
         return StrategyResult(-1, Vertex(0.0f, 0.0f), debugMessage = "Eat Enemy: Not applied")
@@ -19,7 +20,7 @@ class EatEnemyStrategy(val mGlobalConfig: WorldConfig, val mLogger: Logger) : IS
     override fun stopStrategy() {
     }
 
-    private fun searchForEnemies(gameEngine: GameEngine): StrategyResult {
+    private fun searchForEnemies(gameEngine: GameEngine, cachedParseResult: ParseResult?): StrategyResult {
 //TODO: review
         val enemies = gameEngine.worldParseResult.worldObjectsInfo.mEnemies
         val me = gameEngine.worldParseResult.mineInfo
@@ -31,7 +32,16 @@ class EatEnemyStrategy(val mGlobalConfig: WorldConfig, val mLogger: Logger) : IS
                     && (me.mFragmentsState.none { fragment -> (fragment.mCompass.isVertexInDangerArea(it.mVertex)) })
         }.sortedBy { me.getCoordinates().distance(it.mVertex) }
         if (nearEnemies.isNotEmpty()) {
-            val res = StrategyResult(10, gameEngine.getMovementPointForTarget(me.getMainFragment().mId, me.getCoordinates(), nearEnemies[0].mVertex), debugMessage = "Try to eat ${nearEnemies[0].mId}")
+            val chosenEnemy = nearEnemies[0]
+            var targetVertex = chosenEnemy.mVertex
+            cachedParseResult?.let { cache ->
+                val cachedEnemy = cache.worldObjectsInfo.mEnemies.firstOrNull { it.mId == chosenEnemy.mId }
+                cachedEnemy?.let { enemy ->
+                    targetVertex = targetVertex.plus(targetVertex.minus(enemy.mVertex))
+                }
+            }
+
+            val res = StrategyResult(10, gameEngine.getMovementPointForTarget(me.getMainFragment().mId, me.getCoordinates(), targetVertex), debugMessage = "Try to eat ${chosenEnemy.mId}")
             mLogger.writeLog("Enemy strat: ${res}")
             return res
         }
@@ -44,7 +54,16 @@ class EatEnemyStrategy(val mGlobalConfig: WorldConfig, val mLogger: Logger) : IS
                 me.getMinorFragment().canEatEnemyBySplit(it.mMass) && (me.mFragmentsState.none { fragment -> fragment.mCompass.isVertexInDangerArea(it.mVertex) })
             }.sortedBy { me.getCoordinates().distance(it.mVertex) }
             if (nearLowerEnemies.isNotEmpty()) {
-                val res = StrategyResult(10, nearLowerEnemies[0].mVertex, split = true, debugMessage = "Try to eat ${nearLowerEnemies[0].mId}")
+                val chosenEnemy = nearLowerEnemies[0]
+                var targetVertex = chosenEnemy.mVertex
+                cachedParseResult?.let { cache ->
+                    val cachedEnemy = cache.worldObjectsInfo.mEnemies.firstOrNull { it.mId == chosenEnemy.mId }
+                    cachedEnemy?.let { enemy ->
+                        targetVertex = targetVertex.plus(targetVertex.minus(enemy.mVertex))
+                    }
+                }
+
+                val res = StrategyResult(10, chosenEnemy.mVertex, split = true, debugMessage = "Try to eat ${chosenEnemy.mId}")
                 mLogger.writeLog("Enemy 2 strat: ${res}")
                 return res
             }
