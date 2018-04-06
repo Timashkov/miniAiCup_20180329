9568c507fb56cf3e99c792d890e820596a72066d
Alex Timashkov <osb.zla@gmail.com>
2018-04-06 10:22:38 +0300

    added log string

--- strategy-1/src/main/kotlin/strategy/StarBurstStrategy.kt
+++ strategy-1/src/main/kotlin/strategy/StarBurstStrategy.kt
@@ -2,6 +2,7 @@ package strategy
 
 import utils.Logger
 import WorldConfig
+import WorldConfig.Companion.STAR_BURST_DISABLE_TICK
 import data.ParseResult
 import utils.GameEngine
 import utils.Vertex
@@ -12,23 +13,29 @@ class StarBurstStrategy(val mGlobalConfig: WorldConfig, val mLogger: Logger) : I
     // compass and gray sectors
     override fun apply(gameEngine: GameEngine, cachedParseResult: ParseResult?): StrategyResult {
 
-        val tooLateGameStage = gameEngine.currentTick >= mGlobalConfig.GameTicks * 0.3f
+        mLogger.writeLog("Try to apply star burst")
+        val tooLateGameStage = gameEngine.currentTick >= STAR_BURST_DISABLE_TICK
         val fragmentCount = gameEngine.worldParseResult.mineInfo.mFragmentsState.size == 1
+        val isNearEnemies = gameEngine.worldParseResult.worldObjectsInfo.mEnemies.isNotEmpty()
+        val myFragmentCanNotBurst = gameEngine.worldParseResult.mineInfo.getMainFragment().mRadius < mGlobalConfig.VirusRadius * 1.2
 
-        if (gameEngine.worldParseResult.worldObjectsInfo.mEnemies.isNotEmpty() ||
-                tooLateGameStage ||
-                gameEngine.worldParseResult.mineInfo.getMainFragment().mRadius > mGlobalConfig.VirusRadius * 1.2)
+        if (isNearEnemies || tooLateGameStage || myFragmentCanNotBurst ) {
+            mLogger.writeLog("Star burst not applied: $isNearEnemies $tooLateGameStage $myFragmentCanNotBurst\n")
             return StrategyResult(-1, Vertex(0.0f, 0.0f), debugMessage = "Star burst: Not applied")
+        }
+        val mineInfo = gameEngine.worldParseResult.mineInfo
 
-
-        if ( fragmentCount && gameEngine.worldParseResult.mineInfo.getMainFragment().canSplit) {
+        if ( fragmentCount && mineInfo.getMainFragment().canSplit) {
             val nearestViruses = gameEngine.worldParseResult.worldObjectsInfo.mViruses.filter {
-                it.mVertex.distance(gameEngine.worldParseResult.mineInfo.getCoordinates()) <= mGlobalConfig.GameHeight / 2f
-            }.sortedBy { it.mVertex.distance(gameEngine.worldParseResult.mineInfo.getMainFragment().mVertex) }
+                it.mVertex.distance(mineInfo.getCoordinates()) <= mGlobalConfig.GameHeight / 2f
+            }.sortedBy { it.mVertex.distance(mineInfo.getMainFragment().mVertex) }
             if (nearestViruses.isNotEmpty()) {
-                return StrategyResult(2, nearestViruses[0].mVertex)
+                mLogger.writeLog("Go to star $nearestViruses[0].mVertex")
+                return StrategyResult(2, gameEngine.getMovementPointForTarget(mineInfo.getMainFragment().mId, mineInfo.getCoordinates(),nearestViruses[0].mVertex))
             }
         }
+
+        mLogger.writeLog("Can not burst\n")
         return StrategyResult(-1, Vertex(0.0f, 0.0f), debugMessage = "Star burst: Not applied")
     }
 
