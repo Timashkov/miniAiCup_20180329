9b6b866499e0e49e5e862d307f45a37d348fcf05
Alex Timashkov <osb.zla@gmail.com>
2018-04-10 11:33:29 +0300

    fish for new merged strategy

--- strategy-1/src/main/kotlin/strategy/FindFoodStrategyV2.kt
+++ strategy-1/src/main/kotlin/strategy/FindFoodStrategyV2.kt
@@ -1,4 +1,19 @@
 package strategy
 
-class FindFoodStrategyV2 {
+import WorldConfig
+import data.ParseResult
+import utils.GameEngine
+import utils.Logger
+import utils.Vertex
+
+class FindFoodStrategyV2(val mGlobalConfig: WorldConfig, val mLogger: Logger): IStrategy {
+
+
+    override fun apply(gameEngine: GameEngine, cache: ParseResult?): StrategyResult {
+     return StrategyResult(-1, Vertex(0.0f, 0.0f), debugMessage = "FindFoodStrategyV2: Not applied")
+    }
+
+    override fun stopStrategy() {
+
+    }
 }
